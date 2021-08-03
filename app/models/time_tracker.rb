bb973627560ed4b212428b289ce230f77d095356
pEsquerdo <pedroesquerdo87@gmail.com>
2021-08-03 10:26:05 -0300

    added broadcasting for update action

--- app/models/time_tracker.rb
+++ app/models/time_tracker.rb
@@ -1,4 +1,11 @@
 class TimeTracker < ApplicationRecord
+  broadcasts_to ->(time_tracker) { :time_trackers }, inserts_by: :prepend, target: 'time_trackers'
+
+  before_update :set_ended_at
+  def set_ended_at
+    self.ended_at = Time.current if status[/stopped/] && ended_at.nil?
+  end
+
   def running?
     status[/running/]
   end
